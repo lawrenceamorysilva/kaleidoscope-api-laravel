52bc461eb48c38c9360eb6b0b0c78ef47b02798e
Lawrence Silva <lawrences@codev.com>
2025-07-19 12:31:58 +0800

    cors policy update

--- config/cors.php
+++ config/cors.php
@@ -2,13 +2,22 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
     'paths' => ['api/*', 'fallback_login'],
 
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        'https://staging-admin.kaleidoscope.com.au',
-    ],
 
     'allowed_origins_patterns' => ['/^https:\/\/.*\.kaleidoscope\.com\.au$/'],
 
@@ -21,4 +30,3 @@ return [
     'supports_credentials' => false,
 
 ];
-
