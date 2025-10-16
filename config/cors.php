c722b62abb40c75040ef1eaa71381554950caec7
Lawrence Silva <lawrences@codev.com>
2025-10-17 05:39:48 +0800

    tae

--- config/cors.php
+++ config/cors.php
@@ -2,35 +2,28 @@
 
 return [
 
-    'paths' => [
-        'api/*',
-        'auth/*',
-        'fallback_login',
-        'sanctum/csrf-cookie', // ✅ important for stateful + Sanctum
-        'login',
-        'logout'
-    ],
+    'paths' => ['api/*', 'auth/*', 'fallback_login'],
 
-    'allowed_methods' => ['*'], // ✅ allow all HTTP methods
-
-    'allowed_origins' => [
-        // local
+    /*'allowed_origins' => [
+        //local
         'http://api.localhost:8000',
         'http://admin.localhost:4201',
         'http://retailer.localhost:4200',
 
-        // staging
+        //staging
         'https://staging-admin.kaleidoscope.com.au',
         'https://staging-retailer.kaleidoscope.com.au',
         'https://staging-api.kaleidoscope.com.au',
 
-        // prod
+        //prod
         'https://admin.kaleidoscope.com.au',
         'https://retailer.kaleidoscope.com.au',
         'https://api.kaleidoscope.com.au',
-    ],
+    ],*/
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => ['*'], //turn this on when ipad other mobile devices are having issues logging in / apparently only the old ipads are having issues...
+
+    'allowed_origins_patterns' => [], // ✅ Leave empty to prioritize allowed_origins
 
     'allowed_headers' => ['*'],
 
@@ -40,3 +33,4 @@ return [
 
     'supports_credentials' => true,
 ];
+
